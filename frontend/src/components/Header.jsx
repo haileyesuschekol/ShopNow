93e8b638ac2299eeab36a6fd73113ffab8ba04ea
Haileyesus Chekol <teamdevs7@gmail.com>
2024-12-22 15:12:04 +0000

    create route

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,39 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
 import { FaShoppingCart, FaUser } from "react-icons/fa"
 import logo from "../assets/img/shopnowlogo.png"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              alt="logo"
-              width="70rem"
-              height="75rem"
-              style={{ marginLeft: "3px" }}
-            />
-            ShopNow
-          </Navbar.Brand>
+          <Nav.Link as={Link} to="/">
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt="logo"
+                width="70rem"
+                height="75rem"
+                style={{ marginLeft: "3px" }}
+              />
+              ShopNow
+            </Navbar.Brand>
+          </Nav.Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart style={{ marginRight: "5px" }} />
-                Cart
+              <Nav.Link as={Link} to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart style={{ marginRight: "5px" }} />
+                  Cart
+                </Nav.Link>
               </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser style={{ marginRight: "5px" }} />
-                Signin
+              <Nav.Link as={Link} to="/login">
+                <Nav.Link href="/login">
+                  <FaUser style={{ marginRight: "5px" }} />
+                  Signin
+                </Nav.Link>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
