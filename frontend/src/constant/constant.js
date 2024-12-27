f287bb5a2c4516c04d021fc97106e100f0de9d50
Haileyesus Chekol <teamdevs7@gmail.com>
2024-12-27 23:11:07 +0000

    create userApi slice

--- frontend/src/constant/constant.js
+++ frontend/src/constant/constant.js
@@ -4,6 +4,6 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL_DEV
 // ? import.meta.env.BASE_URL_DEV
 //   : import.meta.env.BASE_URL_PROD
 export const PRODUCT_URL = "/api/products"
-export const USER = "/api/users"
+export const USERS_URL = "/api/users"
 export const ORDERS = "/api/orders"
 export const PAYPAL = "/api/config/paypal"
