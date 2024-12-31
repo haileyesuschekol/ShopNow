572ee778c16ee64c130e36505f25a4dd25728129
Haileyesus Chekol <teamdevs7@gmail.com>
2024-12-31 19:34:23 +0000

    create privateRoute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,7 @@ import CartScreen from "./screens/CartScreen.jsx"
 import LoginScreen from "./screens/LoginScreen.jsx"
 import RegisterScreen from "./screens/RegisterScreen.jsx"
 import ShippingScreen from "./screens/ShippingScreen.jsx"
+import PrivateRoute from "./components/PrivateRoute.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,7 +27,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 )
