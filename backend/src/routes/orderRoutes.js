9d6a18e343b2badbf118d857eb7dba3318e72ea3
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-01 20:28:09 +0000

    create get-order-detail api-slice

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -12,7 +12,7 @@ import { protect, admin } from "../middleware/Auth.js"
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route("/my").get(protect, getMyOrders)
-router.route("/:id").get(protect, admin, getMyOrders)
+router.route("/:id").get(protect, getMyOrders)
 router.route("/:id/pay").put(protect, updateOrderToPaid)
 router.route("/:id/deliver").put(protect, admin, updateToDeliverd)
 export default router
--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -10,7 +10,13 @@ export const orderApiSlice = apiSlice.injectEndpoints({
         body: { ...order },
       }),
     }),
+    getOrderDetail: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 })
 
-export const { useCreateOrderMutation } = orderApiSlice
+export const { useCreateOrderMutation, useGetOrderDetailQuery } = orderApiSlice
