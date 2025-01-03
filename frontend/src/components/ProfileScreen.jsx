7069f807d1ee9e27fa1f8c572bea4a8a6a0b0002
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-03 01:43:31 +0000

    create update profile page

--- frontend/src/components/ProfileScreen.jsx
+++ frontend/src/components/ProfileScreen.jsx
@@ -1,1 +1,101 @@
+import { useState, useEffect } from "react"
+import { Table, Form, Button, Row, Col } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify"
+import Message from "./Message"
+import Loader from "./Loader"
+import { useProfileMutation } from "../slices/usersApiSlice"
+import { setCredentials } from "../slices/authSlice"
 
+const ProfileScreen = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const dispatch = useDispatch()
+  const { userInfo } = useSelector((state) => state.auth)
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation()
+
+  useEffect(() => {
+    if (userInfo) {
+      setName(userInfo.name)
+      setEmail(userInfo.email)
+    }
+  }, [userInfo, userInfo.name, userInfo.email])
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      toast.error("Password do not match")
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap()
+        dispatch(setCredentials(res))
+        toast.success("Updated Successfully")
+      } catch (error) {
+        toast.error(error?.data?.message || error.error)
+      }
+    }
+  }
+
+  return (
+    <Row>
+      <h2>User Profile</h2>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="name" className="my-2">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="email" className="my-2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password" className="my-2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="confirmPassword" className="my-2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="my-2">
+          Update
+        </Button>
+        {loadingUpdateProfile && <Loader />}
+      </Form>
+    </Row>
+  )
+}
+
+export default ProfileScreen
