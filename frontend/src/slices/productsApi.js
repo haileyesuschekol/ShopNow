49e28e1c870ea0abb4478921c4e834bf3d890c0f
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-05 11:24:56 +0000

    create upload image api slice

--- frontend/src/slices/productsApi.js
+++ frontend/src/slices/productsApi.js
@@ -1,4 +1,4 @@
-import { PRODUCT_URL } from "../constant/constant"
+import { PRODUCT_URL, UPLOAD_URL } from "../constant/constant"
 import { apiSlice } from "./apiSlice"
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -31,6 +31,13 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Products"],
     }),
+    uploadImage: builder.mutation({
+      query: (data) => ({
+        url: UPLOAD_URL,
+        method: "POST",
+        body: data,
+      }),
+    }),
   }),
 })
 
@@ -39,4 +46,5 @@ export const {
   useGetProductDetailsQuery,
   useCreateProductMutation,
   useUpdateProductMutation,
+  useUploadImageMutation,
 } = productsApiSlice
