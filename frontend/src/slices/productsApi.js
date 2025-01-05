9913c4a3ab72c11f801e572461c4dda9e76cfcb2
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-05 19:27:55 +0000

    create-review-api slice

--- frontend/src/slices/productsApi.js
+++ frontend/src/slices/productsApi.js
@@ -44,6 +44,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         method: "DELETE",
       }),
     }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCT_URL}/${data.productId}/reviews`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Product"],
+    }),
   }),
 })
 
@@ -54,4 +62,5 @@ export const {
   useUpdateProductMutation,
   useUploadImageMutation,
   useDeleteProductMutation,
+  useCreateReviewMutation,
 } = productsApiSlice
